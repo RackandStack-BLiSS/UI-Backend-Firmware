42e56b1d1a914e0549cfa7e78e0f71e0e2f1306c
joban-guron <joban@umich.edu>
2025-04-16 22:39:31 -0400

    Update uart_transmitterV2.py
    updated 4/16

--- thonny/uart_transmitterV2.py
+++ thonny/uart_transmitterV2.py
@@ -4,6 +4,7 @@ import digitalio
 import adafruit_bmp3xx
 import adafruit_scd4x
 import adafruit_hdc302x
+import apds9960
 from DFRobot_Oxygen import *
 import time
 
@@ -17,6 +18,8 @@ scd4x.start_periodic_measurement()
 hdc302x = adafruit_hdc302x.HDC302x(i2c)
 bmp3xx = adafruit_bmp3xx.BMP3XX_I2C(i2c)
 bmp3xx.sea_level_pressure = 1019
+prox = apds9960.APDS9960(i2c)
+prox.enable_proximity = True
 
 # Further initializations for oxygen sensor
 ADDRESS_3    = 0x73
@@ -30,7 +33,7 @@ uart = busio.UART(board.GP16, board.GP17, baudrate=9600, timeout=0)
 UPDATE_INTERVAL = 5.0
 last_time_sent = 0
 
-# configuring RYG output to switches 
+# setting GPIO pins as RYG output 
 redSwitch_pin = digitalio.DigitalInOut(board.GP15)
 redSwitch_pin.direction = digitalio.Direction.OUTPUT
 yellowSwitch_pin = digitalio.DigitalInOut(board.GP14)
@@ -38,50 +41,50 @@ yellowSwitch_pin.direction = digitalio.Direction.OUTPUT
 greenSwitch_pin = digitalio.DigitalInOut(board.GP13)
 greenSwitch_pin.direction = digitalio.Direction.OUTPUT
 
-# inquiring status of rack from rpi4
+# inquiring status of rack from rpi4 through UART
 message_started = False
 message = []
 
-#status = input("What is the rack status? [severe/ok/great] ")
 while True:
     try:
-        # Send sensor values only every UPDATE_INTERVAL seconds.
+        # reading payload status to display RYG lights only every UPDATE_INTERVAL seconds.
         now = time.monotonic()
         if uart.in_waiting > 0:  # Check if data is available in the buffer
-        byte_read = uart.read(1)  # Read one byte
-            
-        if byte_read == b"<":
-                # Start of message. Start accumulating bytes, but don't record the "<".
-            message = []
-            message_started = True
-            continue
+            byte_read = uart.read(1)  # Read one byte
+            if byte_read == b"<":
+                    # Start of message. Start accumulating bytes, but don't record the "<".
+                message = []
+                message_started = True
+                continue
             if message_started:
-                if byte_read == b"g": # if status is good display green light
-                    print("Reading pressure data...")
-                    greenSwitch_pin.value = True  # This sets the GP15 to 3.3V
-            time.sleep(0.5)
-        if status == "severe":
-            redSwitch_pin.value = True  # This sets the GP15 to 3.3V
-            time.sleep(0.5)
-            redSwitch_pin.value = False  # This sets the pin to 0V
-
-            yellowSwitch_pin.value = True  # This sets the GP15 to 3.3V
-            time.sleep(0.5)
-            yellowSwitch_pin.value = False  # This sets the pin to 0V
-
-            greenSwitch_pin.value = True  # This sets the GP15 to 3.3V
-            time.sleep(0.5)
-            greenSwitch_pin.value = False  # This sets the pin to 0V
-
-
+                 if byte_read == b"g": # if status is good display green light
+                    redSwitch_pin.value = False
+                    yellowSwitch_pin.value = False
+                    greenSwitch_pin.value = True  
+                 if byte_read == b"r": # if status is bad display red light
+                    redSwitch_pin.value = True
+                    yellowSwitch_pin.value = False
+                    greenSwitch_pin.value = False  
+                 if byte_read == b"y": # if status is ok display yellow light
+                    redSwitch_pin.value = False
+                    yellowSwitch_pin.value = True
+                    greenSwitch_pin.value =  False 
+            else:
+                # Accumulate message byte.
+                message.append(byte_read.decode("utf-8"))
+        else:
+             time.sleep(5)  # Wait before checking again
+             print(f"No Message Received...")
+        # sending telemetry to rpi4 through UART
         if now - last_time_sent >= UPDATE_INTERVAL:
             uart.write(bytes(f"<p{bmp3xx.pressure}>","ascii"))
             uart.write(bytes(f"<t{hdc302x.temperature}>", "ascii"))
             uart.write(bytes(f"<h{hdc302x.relative_humidity}>", "ascii"))
             uart.write(bytes(f"<d{scd4x.CO2}>", "ascii"))
-           # uart.write(bytes(f"<o{oxygen.get_oxygen_data(COLLECT_NUMBER)}>", "ascii"))
+            uart.write(bytes(f"<x{prox.proximity}>", "ascii"))
+            uart.write(bytes(f"<o{oxygen.get_oxygen_data(COLLECT_NUMBER)}>", "ascii"))
 
-            print("Sending sensor values from Pico 2!")
+            print("Sending sensor values from Pico 1!")
             last_time_sent = now
     except KeyboardInterrupt:
         redSwitch_pin.value = False  # This sets the pin to 0V
@@ -91,3 +94,4 @@ while True:
         break
 
     
+
